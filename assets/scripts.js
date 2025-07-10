4431025ecd6e80b9661bfc1540004bf98aa5d92a
Kaua-Vidal <kauakauavidalpc@gmail.com>
2025-07-10 10:49:30 -0300

    JavaScript DONE

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,11 +1,53 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues() {
     const dolarToday = 5.2
-    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const euroToday = 6.2
 
-    const convertedValue = inputCurrencyValue/dolarToday
-    console.log(convertedValue)
+
+    if (currencySelect.value == 'dolar') {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency", currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+
+    } if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency", currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency", currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+
+
 }
 
+function changeCurrency () {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".img-currency")
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImage.src = "/assets/img/dolar.svg"
+    }
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "/assets/img/euro.svg"
+    }
+
+    convertValues()
+
+}
+
+
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
