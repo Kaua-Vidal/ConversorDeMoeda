5d9e79935e66319f6883240a1ca7d9e0f2e54e76
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2025-07-11 13:37:27 -0300

    adding libra and bitcoin

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,6 +4,8 @@ const currencySelect = document.querySelector(".currency-select")
 function convertValues() {
     const dolarToday = 5.2
     const euroToday = 6.2
+    const libraToday = 7.6
+    const bitcoinToday = 650.866
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
     const currencyValueConverted = document.querySelector(".currency-value")
@@ -18,6 +20,19 @@ function convertValues() {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency", currency: "EUR"
         }).format(inputCurrencyValue / euroToday)
+    }
+    if (currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency", currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
+    }
+    if (currencySelect.value == "bitcoin") {
+
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "decimal", 
+            minimumFractionDigits: 2, 
+            maximumFractionDigits: 2
+        }).format(inputCurrencyValue / bitcoinToday)
     }
 
 
@@ -37,11 +52,19 @@ function changeCurrency () {
 
     if (currencySelect.value == "dolar") {
         currencyName.innerHTML = "Dólar Americano"
-        currencyImage.src = "/assets/img/dolar.svg"
+        currencyImage.src = "./img/dolar.svg"
     }
     if (currencySelect.value == "euro") {
         currencyName.innerHTML = "Euro"
-        currencyImage.src = "/assets/img/euro.svg"
+        currencyImage.src = "./img/euro.svg"
+    }
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./img/libra.svg"
+    }
+    if (currencySelect.value == "bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImage.src = "./img/bitcoin.svg"
     }
 
     convertValues()
